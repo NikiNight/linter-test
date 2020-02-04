93e533f48bf171339b6130d3fba3904ba23b35a3
Viktoriya Vorozhun <viktoriya.vorozhun@gmail.com>
2020-02-04 15:20:11 +0300

    add react app setup

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+const rootElement = document.getElementById('root');
+const element = <div>Hello World!</div>;
+
+render(element, rootElement);
